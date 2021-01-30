be3c4b6fecdc08c4de87cacd3acd13e28b38189f
githubuser402 <colomborin53@gmail.com>
2021-01-30 23:11:12 +0200

    Added file rec. function. Database many to many

--- henbot.py
+++ henbot.py
@@ -6,62 +6,41 @@ from random import choice
 def Bot():
     @bot.message_handler(commands = ['start'])
     def st(message):
-        bot.send_message(message.chat.id, 'Хентай-бот, ось теги: \n #neko \n #milf \n #ass \n #tits \n #milf \n #guro \n #cute')
+        bot.send_message(message.chat.id, 'Це мій невеликий проект, темою якого є хентай-бот. \nБот не має обмежень по кількості тегів. \nКатеорії: \n#ass \n#pussy \n#tits \n#legs \n#neko \n#cute \n#toys \n#penetration \n#anal \n#furry \n#milf \n#creampie \n#ero \nВказівка до використання: категорії потрібно вказувати, розділивши їх пропуском, не використовувати інших знаків.')
     
     @bot.message_handler(content_types = ['photo'])
-    def get_tag(message):
-        if str(message.chat.id) == '559592369' and 'тег:' in message.caption.lower():
+    def get_file(message):
+        if str(message.chat.id) == '559592369':
             if message.caption != None:
-                m = message.caption[4:].strip()
-                if m in tags:
-                    conn = sqlite3.connect('henbot.db')
-                    cursor = conn.cursor()
-                    file_info = bot.get_file(message.photo[-1].file_id)
-                    path = file_info.file_path.split('/')[-1][:-4] + '.txt'
-                    name = file_info.file_id
-                    SQL = cursor.execute('''SELECT file_name FROM txtph''')
-                    conn.commit()
-                    flag = False
-                    for i in SQL.fetchall():
-                        if name == i[0]:
-                            flag = True
-                    if flag == False:
-                        file_d = bot.download_file(file_info.file_path)
-                        with open(p_path + path, 'wb') as f:
-                            f.write(file_d)
-                        cursor.execute('''INSERT INTO txtph(file_path, tag, file_name) VALUES(?, ?, ?);''', (path, m, name))
-                        conn.commit()
-                        bot.reply_to(message, 'Фото завантажено й додано в базу даних')
-                    else:
-                        cursor.execute('''INSERT INTO txtph(file_path, tag, file_name) VALUES(?, ?, ?);''', (path, m, name))
-                        conn.commit()
-                        bot.reply_to(message, 'Фото додано в базу даних, але до цього його вже було завантажено')
-                    conn.close()
-            else:
-                bot.reply_to(message, 'Фото без підпису')
-    
-    @bot.message_handler(content_types = ['text'])
-    def send_file(message):
-        tag = message.text.strip()
-        if tag in tags:
-            try:
                 conn = sqlite3.connect('henbot.db')
                 cursor = conn.cursor()
-                p_arr = cursor.execute('''SELECT file_path FROM txtph WHERE tag = ?;''', (tag,))
+                inscr = sorted(set(message.caption.split(' ')))
+                file_info = bot.get_file(message.photo[-1].file_id)
+                path = file_info.file_path.split('/')[-1][:-3] + 'txt'
+                
+                file_d = bot.download_file(file_info.file_path)
+                with open(p_path + path, 'wb') as f:
+                    f.write(file_d)
+                    
+                cursor.execute('''INSERT INTO files(file_path) VALUES(?)''', (path,))
                 conn.commit()
-                photo_path = choice(p_arr.fetchall())
-                print(photo_path[0])
-                with open(p_path + photo_path[0], 'rb') as f:
-                    p = f.read()
-                    bot.send_photo(message.chat.id, p)
-            except:
-                bot.send_message(message.chat.id, 'Тут поки пусто')
-            finally:
+                l_n = cursor.execute('''SELECT f_id FROM files ORDER BY f_id DESC LIMIT 1''')
+                conn.commit()
+                last_num = l_n.fetchall()
+                
+                for i in inscr:
+                    cursor.execute('''INSERT INTO linking(f_id, t_id) VALUES(?, ?);''', (last_num[0][0], tags[i]))
+                    conn.commit()
+                bot.reply_to(message, 'Фото отримано')
                 conn.close()
+                
+    @bot.message_handler(content_types = ['text'])
+    def send_file(message):
+        pass
         
 if __name__ == '__main__':
     p_path = 'D:/My_packages/Pyprojects/hbot/h_files/'
     bot = telebot.TeleBot('990227536:AAFxnAsTEwCLrFjRsQ-RfpxCAhLAEVCiRao')
-    tags = ['#neko', '#ass', '#tits', '#milf', '#guro', '#cute']
+    tags = {'#ass':1, '#pussy':2, '#tits':3, '#legs':4, '#neko':5, '#cute':6, '#toys':7, '#penetration':8, '#anal':9, '#furry':10, '#milf':11, '#creampie':12, '#ero':13}
     Bot()
     bot.polling()
